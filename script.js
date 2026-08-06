cdd31c85adedde76aada93174c2d72a2651d4eb6
Sony Rajput <sonyrajput2106@gmail.com>
2026-08-06 23:11:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let input = document.getElementById('inputbox');
 let buttons = document.querySelectorAll('button');
 
 
-let string = "";
+let string = "";  
 let arr = Array.from(buttons);
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
